337e4493977c16f4cfd2dc5b2961cb5082045780
babanini95 <arobbanii5@gmail.com>
2025-08-26 06:53:59 +0800

    Set up gemini

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_KEY_API")
+client = genai.Client(api_key=api_key)
+
+
 def main():
-    print("Hello from gentmini!")
+    if len(sys.argv) == 1:
+        print("need argument")
+        sys.exit(1)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=sys.argv[1],
+    )
+    print(response.text)
+    print(
+        f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}"
+    )
 
 
 if __name__ == "__main__":
