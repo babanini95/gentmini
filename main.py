3bb85db3720c8f35fe94606dd7454ddb842c494b
babanini95 <arobbanii5@gmail.com>
2025-08-27 10:21:15 +0800

    Add --verbose argument

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_KEY_API")
 client = genai.Client(api_key=api_key)
+
+messages = [types.Content(role="user", parts=[types.Part(text=sys.argv[1])])]
 
 
 def main():
@@ -15,12 +18,16 @@ def main():
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=sys.argv[1],
+        contents=messages,
     )
     print(response.text)
-    print(
-        f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}"
-    )
+    if len(sys.argv) >= 3:
+        if sys.argv[2] == "--verbose":
+            print(
+                f"User prompt: {sys.argv[1]}\n"
+                f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n"
+                f"Response tokens: {response.usage_metadata.candidates_token_count}"
+            )
 
 
 if __name__ == "__main__":
