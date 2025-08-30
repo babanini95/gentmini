627127717b3ca643880ef76a1457d339adddcb8d
babanini95 <arobbanii5@gmail.com>
2025-08-30 19:17:46 +0800

    Add function to get file content

---
+++ functions/get_file_content.py
@@ -0,0 +1,23 @@
+import os
+import config
+
+
+def get_file_content(working_directory, file_path):
+    abs_working_directory = os.path.abspath(working_directory)
+    abs_file_path = os.path.join(abs_working_directory, file_path)
+
+    if not abs_file_path.startswith(abs_working_directory):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+    if not os.path.isfile(abs_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    try:
+        with open(abs_file_path, "r") as f:
+            file_content_string = f.read(config.MAX_CHARS)
+            if len(file_content_string) == config.MAX_CHARS:
+                file_content_string += (
+                    f'...File "{file_path}" truncated at 10000 characters'
+                )
+            return file_content_string
+    except EOFError as e:
+        return f"Error: {e}"
--- tests.py
+++ tests.py
@@ -1,17 +1,29 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-cases = [
+get_files_info_cases = [
     ("calculator", ".", "current directory"),
     ("calculator", "pkg", "'pkg'"),
     ("calculator", "/bin", "'/bin'"),
     ("calculator", "../", "'../'"),
 ]
 
+get_file_content_cases = [
+    ("calculator", "main.py"),
+    ("calculator", "pkg/calculator.py"),
+    ("calculator", "/bin/cat"),
+    ("calculator", "pkg/does_not_exist.py"),
+]
+
 
 def main():
-    for case in cases:
-        print(f"Result of {case[2]}:")
-        print(get_files_info(case[0], case[1]))
+    # for case in get_files_info_cases:
+    #     print(f"Result of {case[2]}:")
+    #     print(get_files_info(case[0], case[1]))
+
+    for case in get_file_content_cases:
+        print(f"Result of {case[1]}")
+        print(get_file_content(case[0], case[1]))
 
 
 main()
